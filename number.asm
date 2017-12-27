d376ab887496023250d5cdb92a0e42309dbca43e
geethna <geethna.teekey@gmail.com>
2017-12-27 23:24:52 +0530

    first commit

--- number.asm
+++ number.asm
@@ -12,19 +12,23 @@ main:
 	mov ebp,esp
 	sub esp,0x10
 	
-	mov byte[ebp-0xc],0x1
+	mov dword[ebp-0xc],0x0
+	mov dword[ebp-0x8],0x1
 
-
-loop:
 	push dword[ebp-0xc]
 	push number
 	call printf
+
+
+loop:
+	push dword[ebp-0x8]
+	push number
+	call printf
 	
-	mov eax,dword[ebp-0xc]
-	inc eax
-	mov [ebp-0xc],eax
+	mov eax,dword[ebp-0x8]
+	add dword[ebp-0x8],dword[ebp-0xc]
 	
-	cmp eax,101
+	cmp dword[ebp-0x4],5
 	jne loop
 
 	mov esp,ebp
